9da97b2f190300ce733ba2ba1f1e0bb351f4f9f2
Mehmet Ali KIZILARSLAN <76072213+mmokzlrsn@users.noreply.github.com>
2023-08-08 11:39:41 +0300

    enrolment model class variables were added

--- ContosoUniversity/ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/ContosoUniversity/Models/Student.cs
@@ -7,7 +7,7 @@ namespace ContosoUniversity.Models
 {
     public class Student
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime EnrollmentDate { get; set; }
