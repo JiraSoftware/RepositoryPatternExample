04a9654527da55a26028436bca3e83d328d18e46
Mehmet Ali KIZILARSLAN <76072213+mmokzlrsn@users.noreply.github.com>
2023-08-08 11:48:53 +0300

    Create SchoolContext.cs
    derive from DbContext

---
+++ ContosoUniversity/ContosoUniversity/DAL/SchoolContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("SchoolContext")
+        {
+        }
+    }
+}
