c029f0119a91ae5f1f05fb86562d1dbfe5dee6c0
Mehmet Ali KIZILARSLAN <76072213+mmokzlrsn@users.noreply.github.com>
2023-08-08 11:48:08 +0300

    course model variables
    course model class variables were added

--- ContosoUniversity/ContosoUniversity/Models/Course.cs
+++ ContosoUniversity/ContosoUniversity/Models/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,12 @@ namespace ContosoUniversity.Models
 {
     public class Course
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)] // Basically, this attribute lets you enter the primary key for the course rather than having the database generate it.
+        public int CourseId { get; set; } //primary key
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; } //one to many
+
     }
 }
